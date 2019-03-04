c82c8e165864f1db476fcfc7512d99036956aa90
Cloud User <ec2-user@ip-10-0-3-248.us-west-2.compute.internal>
2019-03-04 18:23:48 +0000

    1st commit

---
+++ core/src/test/java/testpackage/SampleTest.java
@@ -0,0 +1,13 @@
+package testpackage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleTest {
+
+    @Test
+    public void testSomething() {
+        assertTrue(1 == 1);
+    }
+}
