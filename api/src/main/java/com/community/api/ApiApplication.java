a1a020f8acd4814e2701a7abead7fb5de90d83b5
yuriimatysik <yuriimatysik@gmail.com>
2019-03-19 21:47:48 +0200

    Update to run on tomcat

--- api/src/main/java/com/community/api/ApiApplication.java
+++ api/src/main/java/com/community/api/ApiApplication.java
@@ -5,11 +5,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author Elbert Bautista (elbertbautista)
  */
 @SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
 @EnableAutoConfiguration
 public class ApiApplication {
 
--- site/src/main/java/com/community/SiteApplication.java
+++ site/src/main/java/com/community/SiteApplication.java
@@ -5,8 +5,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
 @EnableAutoConfiguration
 public class SiteApplication {
 
